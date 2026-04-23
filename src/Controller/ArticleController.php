4c21a67cfc50134137aa849435e43c4a3acc4841
TheotimALBERTEAU <149970439+TheotimALBERTEAU@users.noreply.github.com>
2026-04-23 23:14:39 +0200

    🚀 up : html/css | ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,11 +8,17 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
+    #[Route('/articles', name: 'app_article')]
     public function index(): Response
     {
+        $articles = [
+            ['id' => 1, 'title' => 'Premier Article', 'desc' => 'Description du premier article', 'author' => 'Isaac', 'imgPath' => 'https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-653001154-e1691965000531.jpg'],
+            ['id' => 2, 'title' => 'Deuxième Article', 'desc' => 'Description du deuxième article', 'author' => 'Sanchez', 'imgPath' => 'https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-653001154-e1691965000531.jpg'],
+            ['id' => 3, 'title' => 'Troisième Article', 'desc' => 'Description du troisième article', 'author' => 'Toto', 'imgPath' => 'https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-653001154-e1691965000531.jpg'],
+        ];
+
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'liste_articles' => $articles,
         ]);
     }
 }
